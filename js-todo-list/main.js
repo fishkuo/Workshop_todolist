bac0be2f7914a90edc3554919a797ae73bad1adb
Fish Kuo <fishkuo0101@gmail.com>
2020-12-08 16:01:57 +0800

    drag and drop

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -17,10 +17,23 @@ window.addEventListener('DOMContentLoaded', function () {
 
 	// add-items
 	function addItem(input) {
-		let item = document.createElement('li');
-		item.innerHTML = `${input}<span class="close">x</span>`;
+		let node = [];
+		let div = document.createElement('div');
+		div.setAttribute('class', 'drop-zone');
+		let li = document.createElement('li');
+		li.setAttribute('id', 'myDraggableElement');
+		li.setAttribute('draggable', 'true');
+		li.appendChild(document.createTextNode(input));
+		let span = document.createElement('span');
+		span.appendChild(document.createTextNode('x'));
+		span.setAttribute('class', 'close');
+		li.appendChild(span);
+		div.appendChild(li);
+
+		let item = div;
+
 		item.addEventListener('click', function () {
-			this.classList.toggle('checked');
+			this.querySelector('li').classList.toggle('checked');
 		});
 		item.querySelector('span').addEventListener('click', function () {
 			item.style.display = 'none';
@@ -30,6 +43,29 @@ window.addEventListener('DOMContentLoaded', function () {
 	const addButton = document.querySelector('#addBtn');
 	addButton.addEventListener('click', function () {
 		let userInput = document.querySelector('#input').value;
+		document.querySelector('#input').value = '';
 		document.querySelector('ul').insertAdjacentElement('afterbegin', addItem(userInput));
+		drag();
 	});
+
+	// drag-items
+	function drag() {
+		document.querySelectorAll('#myDraggableElement').forEach((dragItem) => {
+			dragItem.addEventListener('dragstart', (e) => {
+				e.dataTransfer.setData('text/plain', dragItem.id);
+				console.log(e);
+			});
+			document.querySelectorAll('.drop-zone').forEach((dropZone) => {
+				dropZone.addEventListener('dragover', (e) => {
+					e.preventDefault();
+				});
+				dropZone.addEventListener('drop', (e) => {
+					e.preventDefault();
+					const droppedElementId = e.dataTransfer.getData('text/plain');
+					const droppedElement = document.getElementById(`${droppedElementId}`);
+					dropZone.appendChild(droppedElement);
+				});
+			});
+		});
+	}
 });
