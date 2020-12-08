17d92ffcd9031c131956427c3ba099372b38dfa7
Fish Kuo <fishkuo0101@gmail.com>
2020-12-08 12:11:21 +0800

    add items delete

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -1,10 +1,17 @@
 // TO DO
 
-// items-completed
 window.addEventListener('DOMContentLoaded', function () {
+	// items-completed
 	let items = document.querySelectorAll('ul li').forEach((item) => {
 		item.addEventListener('click', function () {
 			item.classList.toggle('checked');
+		});
+	});
+
+	// items-delete
+	let crosses = document.querySelectorAll('ul li span').forEach((cross) => {
+		cross.addEventListener('click', function () {
+			cross.parentElement.style.display = 'none';
 		});
 	});
 });
