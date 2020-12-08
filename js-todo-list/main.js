77b7b61da1081bddf5979a61f3b7f19050a059f9
Fish Kuo <fishkuo0101@gmail.com>
2020-12-08 12:49:54 +0800

    add items

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -2,16 +2,34 @@
 
 window.addEventListener('DOMContentLoaded', function () {
 	// items-completed
-	let items = document.querySelectorAll('ul li').forEach((item) => {
+	document.querySelectorAll('ul li').forEach((item) => {
 		item.addEventListener('click', function () {
 			item.classList.toggle('checked');
 		});
 	});
 
 	// items-delete
-	let crosses = document.querySelectorAll('ul li span').forEach((cross) => {
+	document.querySelectorAll('ul li span').forEach((cross) => {
 		cross.addEventListener('click', function () {
 			cross.parentElement.style.display = 'none';
 		});
+	});
+
+	// add-items
+	function addItem(input) {
+		let item = document.createElement('li');
+		item.innerHTML = `${input}<span class="close">x</span>`;
+		item.addEventListener('click', function () {
+			this.classList.toggle('checked');
+		});
+		item.querySelector('span').addEventListener('click', function () {
+			item.style.display = 'none';
+		});
+		return item;
+	}
+	const addButton = document.querySelector('#addBtn');
+	addButton.addEventListener('click', function () {
+		let userInput = document.querySelector('#input').value;
+		document.querySelector('ul').insertAdjacentElement('afterbegin', addItem(userInput));
 	});
 });
